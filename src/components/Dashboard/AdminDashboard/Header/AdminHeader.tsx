616df8379dc938e3751572296eeac3e9dd485d0a
saix511 <saadirfan41@gmail.com>
2023-09-05 03:38:28 +0500

    reassign candidates

--- src/components/Dashboard/AdminDashboard/Header/AdminHeader.tsx
+++ src/components/Dashboard/AdminDashboard/Header/AdminHeader.tsx
@@ -1,8 +1,55 @@
 import { Progress } from '@/components/ui/progress'
 import { RegisterUserDialogButton } from './CreateUser'
 import { queryClient } from '@/main'
+import { useQuery } from '@tanstack/react-query'
+import { useAuthStore } from '@/store/useAuthStore'
 
 const AdminHeader = () => {
+  const { access_token } = useAuthStore()
+
+  const reassignCandidate = async () => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/reassign`,
+        {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      const recruiters = await res.json()
+      console.log(recruiters)
+
+      if (res.status === 401) {
+        return { message: 'Not authenticated' }
+      }
+
+      return recruiters
+    } catch (error: any) {
+      return { message: 'Internal Server Error' }
+    }
+  }
+
+  const { isFetching, error, refetch } = useQuery({
+    enabled: false,
+    queryKey: ['reassign-candidate'],
+    queryFn: reassignCandidate, // Use the defined function here
+  })
+  if (error) return <p className=' text-base text-[#69C920]'>Error</p>
+  // if (data.message) {
+  //   if (data.message === 'Not authenticated')
+  //     return (
+  //       <p className=' text-base text-[#69C920]'>Login Credentials Invalid</p>
+  //     )
+  //   return <p className=' text-base text-[#69C920]'>{data.message}</p>
+  // }
+  // if (data?.detail == 'No Candidate Found') {
+  //   return <p className=' text-base text-[#69C920]'> No Application Found</p>
+  // }
+  console.log(isFetching)
+
   return (
     <header className=' flex gap-x-10 pr-12 py-3 '>
       <div className='flex w-full  items-center'>
@@ -10,11 +57,15 @@ const AdminHeader = () => {
           <RegisterUserDialogButton />
 
           <button
-            onClick={() =>
-              queryClient.invalidateQueries({
-                queryKey: ['recruiters-data'],
-              })
-            }
+            disabled={isFetching}
+            onClick={() => {
+              refetch(),
+                setTimeout(() => {
+                  queryClient.invalidateQueries({
+                    queryKey: ['recruiters-data'],
+                  })
+                }, 0)
+            }}
             className={
               'bg-gradient-to-r  focus:outline-none from-[#6bf4a4] from-0% to-[#34ceff] to-100% text-white text-sm  flex items-center gap-x-2 rounded-sm p-1 px-2 '
             }
